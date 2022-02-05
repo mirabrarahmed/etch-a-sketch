4d5da6fde10a4ba528ef2f4efa2450b898a94ade
Mir Abrar Ahmed <abrarahmedshawn@gmail.com>
2022-02-06 02:41:58 +0600

    Etch-A-Sketch final

---
+++ etch-a-sketch.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const container = document.querySelector('#container')
+
+function createGrid(number) {
+	let h = 640 / number
+	let w = 640 / number
+	for(let rows=1; rows<=number; rows++ ){
+		for(let columns=1; columns<=number; columns++){
+			let div = document.createElement('div')
+			div.classList.add('grid')
+			let grid = document.getElementsByClassName("grid")
+			div.setAttribute('style', `height: ${h}px; width: ${w}px`)
+			container.appendChild(div)
+		};
+	};
+};
+
+createGrid(16)
+paintGrid()
+
+function getDimensions(){
+	let dimension = prompt('enter dimension:')
+	createGrid(dimension)
+	paintGrid();
+}
+
+function paintGrid(){
+	const grids = document.querySelectorAll('.grid')
+
+	grids.forEach((grid) => {
+		grid.addEventListener('mouseenter', (e) => {
+			grid.style.background = 'black';
+		})
+	})
+}
+
+
+
+const reset = document.querySelector("#reset");
+
+reset.addEventListener("click", (e) => {
+	const divs = document.querySelectorAll(".grid")
+	divs.forEach((div) => {
+		container.removeChild(div);
+	})
+	getDimensions();
+})
